a9f4d48caf5f4fb88c6e191f17440064ded332c3
Richa-2309 <165482415+Richa-2309@users.noreply.github.com>
2026-09-09 23:27:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 
+package com.example.todo.model;
+
+public class Todo {
+
+    private Integer id;
+    private String title;
+    private String description;
+    private Boolean completed;
+
+    public Todo() {
+    }
+
+    public Todo(Integer id, String title, String description, Boolean completed) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.completed = completed;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(Boolean completed) {
+        this.completed = completed;
+    }
+}
