518b079afe056639eb6329f2b699a238f5dc1468
Richa-2309 <165482415+Richa-2309@users.noreply.github.com>
2026-09-12 23:31:09 +0530

    Add getter and setter for title in Todo class
    Restored getter and setter methods for the title property in the Todo class to allow access and modification of the title.

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 
 package com.example.todo.model;
 
+/**
+ * Represents a Todo item.
+ */
 public class Todo {
 
+    // Unique identifier for the todo
     private Integer id;
+
+    // Short title of the todo
     private String title;
+
+    // Detailed description of the todo
     private String description;
+
+    // Indicates whether the todo is completed
     private Boolean completed;
 
+    /**
+     * Default constructor.
+     */
     public Todo() {
     }
 
+    /**
+     * Creates a Todo with all properties.
+     *
+     * @param id          unique todo identifier
+     * @param title       todo title
+     * @param description todo description
+     * @param completed   completion status
+     */
     public Todo(Integer id, String title, String description, Boolean completed) {
         this.id = id;
         this.title = title;
@@ -18,35 +39,72 @@ public class Todo {
         this.completed = completed;
     }
 
+    /**
+     * Returns the todo ID.
+     *
+     * @return todo ID
+     */
     public Integer getId() {
         return id;
     }
 
-    // public void setId(Integer id) {
-    //     this.id = id;
-    // }
-
-    // public String getTitle() {
-    //     return title;
-    // }
-
-    // public void setTitle(String title) {
-    //     this.title = title;
-    // }
-
+    /**
+     * Returns the todo description.
+     *
+     * @return todo description
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Updates the todo description.
+     *
+     * @param description new todo description
+     */
     public void setDescription(String description) {
         this.description = description;
     }
 
+    /**
+     * Returns the completion status.
+     *
+     * @return true if completed, otherwise false
+     */
     public Boolean getCompleted() {
         return completed;
     }
 
+    /**
+     * Updates the completion status.
+     *
+     * @param completed new completion status
+     */
     public void setCompleted(Boolean completed) {
         this.completed = completed;
     }
 }
+```
+
+### Important observation
+
+Your `title` field currently has **no getter or setter** because you commented them out:
+
+```java
+// public String getTitle() {
+//     return title;
+// }
+```
+
+So if your controller/service needs to access the title, you'll need:
+
+```java
+public String getTitle() {
+    return title;
+}
+
+public void setTitle(String title) {
+    this.title = title;
+}
+
+
